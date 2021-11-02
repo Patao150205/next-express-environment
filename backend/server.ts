694b65425f2e7b43e72e82dd7cfc9f3423460d69
Patao150205 <imaimizuki150205@yahoo.co.jp>
2021-11-03 08:23:49 +0900

    implement hot reload

--- backend/server.ts
+++ backend/server.ts
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
-	res.send('ぱたこ');
+	res.send('ぱたお');
 })
 
 app.use('/api', require('./api/hello'));
