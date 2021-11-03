fd2bcfd1c93e36f7bceee110b48a4dd686ae1dd2
Patao150205 <imaimizuki150205@yahoo.co.jp>
2021-11-03 13:06:54 +0900

    implement node container

---
+++ backend/server/build/server.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var app = (0, express_1.default)();
+var PORT = process.env.PORT || 8000;
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+app.get('/', function (req, res) {
+    res.send('ぱたお');
+});
+app.use('/api', require('./api/hello'));
+app.listen(PORT, function () {
+    console.log("express start on " + PORT);
+});
